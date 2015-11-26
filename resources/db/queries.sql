96b94c4d29f1704a3d81c4709fb7740d932a0809
Ray Miller <ray@1729.org.uk>
2015-11-26 22:55:11 +0000

    Delete unused queries.sql

--- resources/db/queries.sql
+++
@@ -1,4 +0,0 @@
--- :name q-create-ad :query :one
--- :doc Insert an ad into the ads table
-INSERT INTO ads(title, content, width, height, url, size, content_type, image)
-VALUES(:title, :content, :width, :height, :url, :size, :content-type, :image)
